3bb0150e36c98123fdfd2bb9082017c494ebfcb1
baclark77 <baclark77@yahoo.com>
2014-04-02 12:08:37 -0400

    1. added new handler wait for logout test to be more reliable. 2. moved the program guide configuration over to the program guide test.

--- main/src/test/java/com/nbcuni/test/publisher/pageobjects/Logout.java
+++ main/src/test/java/com/nbcuni/test/publisher/pageobjects/Logout.java
@@ -1,10 +1,12 @@
 package com.nbcuni.test.publisher.pageobjects;
 
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.Reporter;
 import com.nbcuni.test.publisher.common.Config;
 import com.nbcuni.test.publisher.common.Driver.Driver;
@@ -37,29 +39,41 @@ public class Logout {
     public void ClickLogoutBtn() throws Exception {
     	
     	Reporter.log("Click the 'Logout' button.");
-    	//existing chrome bug where logout link click doesn't work properly
-    	if (config.getConfigValue("Browser").equals("chrome")) {
-    		webDriver.manage().deleteAllCookies();
-    		webDriver.navigate().to(config.getConfigValue("AppURL"));
-    	}
-    	else {
+    	Thread.sleep(1000); //slight pause required here
+    	webDriver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+		for (int second = 0; ; second++) {
+    		
+    		if (second >= 30) {
+                Assert.fail("Failed to logout after timeout");
+            }
+    		
+    		Boolean logoutLnkPresent = true;
     		try {
-        		LogOut_Btn.click();
-        		
-        	}
-        	catch (WebDriverException e) {
-        		webDriver.executeScript("arguments[0].click();", LogOut_Btn);
-        		webDriver.navigate().refresh(); //TODO - logout requires a refresh for some reason. Figure out a better way
-            	
-        	}
-        	
-    	}
+    			LogOut_Btn.getText();
+    			logoutLnkPresent = true;
+    		}
+    		catch (WebDriverException e) {
+    			logoutLnkPresent = false;
+    		}
+    		
+            if (logoutLnkPresent == true) {
+            	try {
+            		LogOut_Btn.click();
+            		
+            	}
+            	catch (WebDriverException e) {
+            		webDriver.executeScript("arguments[0].click();", LogOut_Btn);
+            		
+            	}
+            }
+            else {
+            	break;
+            }
+            Thread.sleep(500);
+        }
+    	webDriver.manage().timeouts().implicitlyWait(config.getImplicitWaitTime(), TimeUnit.SECONDS);
     	
     }
     
-    
-    
-    
-  
 }
 
--- main/src/test/java/com/nbcuni/test/publisher/tests/ContentEntityCreationManagement/ProgramGuides/ROVITVSchedule/ProgramGuideVerification.java
+++ main/src/test/java/com/nbcuni/test/publisher/tests/ContentEntityCreationManagement/ProgramGuides/ROVITVSchedule/ProgramGuideVerification.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import com.nbcuni.test.publisher.common.ParentTest;
 import com.nbcuni.test.publisher.common.RerunOnFailure;
 import com.nbcuni.test.publisher.pageobjects.Blocks;
+import com.nbcuni.test.publisher.pageobjects.Modules;
 import com.nbcuni.test.publisher.pageobjects.UserLogin;
 import com.nbcuni.test.publisher.pageobjects.Configuration.ProgramGuide;
 import com.nbcuni.test.publisher.pageobjects.Content.ContentParent;
@@ -13,19 +14,30 @@ public class ProgramGuideVerification extends ParentTest{
 	/*************************************************************************************
      * TEST CASE 
      * Step 1 - Log in to the test instance as Drupal User 1 (usually admin in Publisher sites)./<br>
-     * Step 2 - Verify that the section, "Program Guide" is displayed in the site top page.<br>
-     * Step 3 - Verify that a table appears underneath it with the columns, "Date", "Show", "Information".
-     * Step 4 - NA = Verify that only 1 row appears in the table, "There is no data".
-     * Step 5 - Click on "Home" --> "Run Cron".<br>
-     * Step 6 - Scroll down to the "Program Guide Data URL" field, and verify that it is populated with 
+     * Step 2 - Populate the field, "Filter list" with the value, "program guide", put a check on the "ENABLED"
+     * 			checkbox next to the "Program Guide" and "Program Guide Example" modules, and click on the "Save configuration" button.<br>
+     * Step 3 - Populate the field, "Filter list" with the value, "views ui", put a check on the "ENABLED" checkbox 
+     * 			next to the "Views UI" module, and click on the "Save configuration" button.
+     * Step 4 - Click on Structure --> Blocks.
+     * Step 5 - Scroll down to the "Program Guide" instance, and select the "Content" for the "REGION" value, 
+     * 			and click on the "Save blocks" button.<br>
+     * Step 6 - Click on "Configuration" --> "Web services" --> "Program Guide".
+     * Step 7 - Populate the field, "Data URL" with the value,
      * 			"http://feed.entertainment.tv.theplatform.com/f/dCK2IC/stage_usa_listing?range=1-*&form=json".<br>
-     * Step 7 - Click on the "Home" icon.<br>
-     * Step 8 - Verify that the section, "Program Guide" appears.
-     * Step 9 - Verify that a table appears underneath it with the columns, "Date", "Show", "Information".<br>
-     * Step 10 - Verify that the row, "No data found" does not appear anymore.<br>    
-     * Step 11 - Verify that TV Listings information appears underneath the "Date", "Show" and "Information" column starting from the current date.
-     * Step 12 - Scroll down, and verify that the TV Listings information appears for the next day also.
-	 * Step 13 - Disable Program Guide block
+     * Step 8 -  Save and verify success message.
+     * Step 9 - Verify that the section, "Program Guide" is displayed in the site top page.<br>
+     * Step 10 - Verify that a table appears underneath it with the columns, "Date", "Show", "Information".
+     * Step 11 - NA = Verify that only 1 row appears in the table, "There is no data".
+     * Step 12 - Click on "Home" --> "Run Cron".<br>
+     * Step 13 - Scroll down to the "Program Guide Data URL" field, and verify that it is populated with 
+     * 			"http://feed.entertainment.tv.theplatform.com/f/dCK2IC/stage_usa_listing?range=1-*&form=json".<br>
+     * Step 14 - Click on the "Home" icon.<br>
+     * Step 15 - Verify that the section, "Program Guide" appears.
+     * Step 16 - Verify that a table appears underneath it with the columns, "Date", "Show", "Information".<br>
+     * Step 17 - Verify that the row, "No data found" does not appear anymore.<br>    
+     * Step 18 - Verify that TV Listings information appears underneath the "Date", "Show" and "Information" column starting from the current date.
+     * Step 19 - Scroll down, and verify that the TV Listings information appears for the next day also.
+	 * Step 20 - Disable Program Guide block
      * @throws Throwable No Return values are needed
      *************************************************************************************/
 	@Test(retryAnalyzer = RerunOnFailure.class, groups = {"full" })
@@ -34,47 +46,81 @@ public class ProgramGuideVerification extends ParentTest{
 		//Step 1
 		UserLogin userLogin = applib.openApplication();
 		userLogin.Login(applib.getAdmin1Username(), applib.getAdmin1Password());
-	    
-	    //Step 2
-	    ProgramGuide programGuide = new ProgramGuide(webDriver);
-        programGuide.VerifyProgramGuideText();
+			    
+		//Step 2 and 3	   
+		Modules modules = new Modules(webDriver, applib);
+		taxonomy.NavigateSite("Modules");
+		overlay.SwitchToActiveFrame();
+		modules.EnterFilterName("Program Guide");
+		modules.EnableModule("Program Guide");
+		modules.EnterFilterName("Program Guide Example");
+		modules.EnableModule("Program Guide Example");
+		modules.EnterFilterName("Views UI");
+		modules.EnableModule("Views UI");
+		overlay.ClickCloseOverlayLnk();
+			    
+		//Step 4
+		taxonomy.NavigateSite("Structure>>Blocks");        
+		overlay.SwitchToActiveFrame();
+		        
+		//Step 5
+		Blocks blocks = new Blocks(webDriver);
+		blocks.SelectRegion("Program Guide", "Content");
+		blocks.ClickSaveBlocksBtn();
+		overlay.SwitchToActiveFrame();
+		ContentParent contentParent = new ContentParent(webDriver, applib);
+		contentParent.VerifyMessageStatus("The block settings have been updated.");
+		        
+		//Step 6
+		blocks.VerifySelectedRegion("Program Guide", "Content");
+		overlay.ClickCloseOverlayLnk();
+		taxonomy.NavigateSite("Configuration>>Web services>>Program Guide");
+		      
+		//Step 7
+		overlay.SwitchToActiveFrame();
+		ProgramGuide programGuide = new ProgramGuide(webDriver);
+		programGuide.EnterDataURL();
+		      
+		//Step 8
+		programGuide.ClickSaveConfigBtn();
+		contentParent.VerifyMessageStatus("The configuration options have been saved.");      
+		overlay.ClickCloseOverlayLnk();
+		        
+		//Step 9
+	    programGuide.VerifyProgramGuideText();
         
-        //Step 3
+        //Step 10
         programGuide.VerifyDateShowInfoColumn();   
         
-        //Step 4 - N/A
+        //Step 11 - N/A
         
-        //Step 5
+        //Step 12
         taxonomy.NavigateSite("Home>>Run cron");
         overlay.SwitchToActiveFrame();
-	    ContentParent contentParent = new ContentParent(webDriver, applib);
 	    contentParent.VerifyMessageStatus("Cron ran successfully.");
-        
-        //Step 6
+	    
+        //Step 13
         programGuide.ProgramGuideRunCronStatus();
         overlay.ClickCloseOverlayLnk();
         
-        //Step 7
+        //Step 14
         taxonomy.NavigateSite("Home");
         
-        //Step 8
+        //Step 15
         programGuide.VerifyProgramGuideText();
         
-        //Step 9
+        //Step 16
         programGuide.VerifyDateShowInfoColumn();
         
-        //Step 10 through 12 (truncated)
+        //Step 17 and 18 (truncated)
         programGuide.VerifyProgramGuideContainsShows();
         
-        //Step 13
+        //Step 19
         taxonomy.NavigateSite("Structure>>Blocks");
         overlay.SwitchToActiveFrame();
-        Blocks blocks = new Blocks(webDriver);
         blocks.SelectRegion("Program Guide", "- None -");
         blocks.ClickSaveBlocksBtn();
         
     }
     
-   
-
 }
