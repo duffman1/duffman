52d7d61044b14587e7158281f2a9b896e70956b3
baclark77 <baclark77@yahoo.com>
2014-06-24 14:12:17 -0400

    1. added new content build script for mpx content creation and performance testing while multiple users are creating content and hitting the site.

---
+++ main/src/test/java/com/nbcuni/test/publisher/contentbuildscripts/MPXPerformanceUpgrade/CreateVideosAndPub7Content/OpenHomePage3.java
@@ -0,0 +1,21 @@
+package com.nbcuni.test.publisher.contentbuildscripts.MPXPerformanceUpgrade.CreateVideosAndPub7Content;
+
+import com.nbcuni.test.publisher.common.ParentTest;
+import com.nbcuni.test.publisher.common.RerunOnFailure;
+import org.testng.annotations.Test;
+
+public class OpenHomePage3 extends ParentTest{
+	
+    @Test(retryAnalyzer = RerunOnFailure.class, groups = {"full", "smoke", "mpx"})
+    public void OpenHomePage_Test() throws Exception {
+
+    	//Continuously open the home page indefinitely
+    	for(int CCount=0;CCount<1000;CCount++){
+    		applib.openApplication();
+    		
+    		webDriver.navigate().to(applib.getApplicationURL());
+    		
+    	}
+    	
+    }
+}
