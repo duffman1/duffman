dfbccc5454ef228ce2f412599b1589d9cb8ac3c4
baclark77 <baclark77@yahoo.com>
2014-07-07 11:39:30 -0400

    1. added new surf module test and associated page object.

---
+++ main/src/test/java/com/nbcuni/test/publisher/pageobjects/Surf.java
@@ -0,0 +1,110 @@
+package com.nbcuni.test.publisher.pageobjects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.Reporter;
+import com.nbcuni.test.publisher.common.Driver.Driver;
+
+/*********************************************
+ * publisher.nbcuni.com Surf Library. Copyright
+ * 
+ * @author Brandon Clark
+ * @version 1.0 Date: July 7, 2014
+ *********************************************/
+
+public class Surf {
+
+    private Driver webDriver;
+    private WebDriverWait wait;
+    
+    //PAGE OBJECT CONSTRUCTOR
+    public Surf(Driver webDriver) {
+        this.webDriver = webDriver;
+        PageFactory.initElements(webDriver, this);
+        wait = new WebDriverWait(webDriver, 30);
+    }
+    
+    //PAGE OBJECT IDENTIFIERS
+    @FindBy(how = How.ID, using = "action-signin")
+    private WebElement SignIn_Btn;
+    
+    @FindBy(how = How.XPATH, using = "//iframe[contains(@id, 'easyXDM')]")
+    private WebElement SurfContainer_Frm;
+    
+    @FindBy(how = How.ID, using = "display-frame")
+    private WebElement SurfLogin_Frm;
+    
+    @FindBy(how = How.ID, using = "input_username")
+    private WebElement EmailOrUsername_Txb;
+    
+    @FindBy(how = How.ID, using = "input_password")
+    private WebElement Password_Txb;
+    
+    @FindBy(how = How.ID, using = "button_submit")
+    private WebElement SignInF_Btn;
+    
+    @FindBy(how = How.ID, using = "action-edit")
+    private WebElement EditYourProfile_Btn;
+    
+    @FindBy(how = How.ID, using = "action-signout")
+    private WebElement SignOut_Btn;
+    
+    @FindBy(how = How.ID, using = "surf-info")
+    private WebElement SurfInfo_Ctr;
+    
+    
+    //PAGE OBJECT METHODS
+    public void ClickSignInBtn() throws Exception {
+    	
+    	Reporter.log("Click the 'Sign-in' button.");
+    	wait.until(ExpectedConditions.visibilityOf(SignIn_Btn)).click();
+    }
+    
+    public void SwitchToSurfFrm() throws Exception {
+    	
+    	Reporter.log("Switch to the Surf frame.");
+    	webDriver.switchTo().frame(SurfContainer_Frm);
+    	webDriver.switchTo().frame(SurfLogin_Frm);
+    }
+    
+    public void EnterEmailUsername(String userName) throws Exception {
+    	
+    	Reporter.log("Enter '" + userName + "' in the 'Email or Username' text box.");
+    	EmailOrUsername_Txb.sendKeys(userName);
+    }
+    
+    public void EnterPassword(String password) throws Exception {
+    	
+    	Reporter.log("Enter '" + password + "' in the 'Password' text box.");
+    	Password_Txb.sendKeys(password);
+    }
+    
+    public void ClickSurfFrmSignInBtn() throws Exception {
+    	
+    	Reporter.log("Click the 'Sign In' button.");
+    	SignInF_Btn.click();
+    }
+    
+    public void VerifyUsernamePresent(String userName) throws Exception {
+    	
+    	Reporter.log("Verify surf username '" + userName + "' is present.");
+    	wait.until(ExpectedConditions.visibilityOf(SurfInfo_Ctr));
+    	Assert.assertTrue(SurfInfo_Ctr.getText().contains(userName));
+    }
+    
+    public void ClickSignOutBtn() throws Exception {
+    	
+    	Reporter.log("Click the 'Sign Out' button.");
+    	wait.until(ExpectedConditions.visibilityOf(SignOut_Btn)).click();
+    	
+    	Reporter.log("Verify the 'Sign In' button is present.");
+    	wait.until(ExpectedConditions.visibilityOf(SignIn_Btn));
+    }
+    
+}
+
---
+++ main/src/test/java/com/nbcuni/test/publisher/tests/SiteManagementAndReporting/SURF/SURFModule.java
@@ -0,0 +1,87 @@
+package com.nbcuni.test.publisher.tests.SiteManagementAndReporting.SURF;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+import com.nbcuni.test.publisher.common.ParentTest;
+import com.nbcuni.test.publisher.common.RerunOnFailure;
+import com.nbcuni.test.publisher.pageobjects.Blocks;
+import com.nbcuni.test.publisher.pageobjects.Modules;
+import com.nbcuni.test.publisher.pageobjects.Surf;
+import com.nbcuni.test.publisher.pageobjects.UserLogin;
+
+public class SURFModule extends ParentTest {
+
+	/*************************************************************************************
+	 * TEST CASE - TC3564
+     * Steps - https://rally1.rallydev.com/#/14663927728d/detail/testcase/19953324533
+	 *************************************************************************************/
+	 @Test(retryAnalyzer = RerunOnFailure.class, groups = {"full"})
+	 public void SURFModule_TC3564() throws Exception {
+		 
+		Reporter.log("STEP 1 - N/A");
+		
+		Reporter.log("STEP 2");
+		UserLogin userLogin = applib.openApplication();
+		userLogin.Login(applib.getAdmin1Username(), applib.getAdmin1Password());
+	       
+		Reporter.log("STEP 3 - 9");
+		taxonomy.NavigateSite("Modules");
+		overlay.SwitchToActiveFrame();
+		Modules modules = new Modules(webDriver, applib);
+		modules.EnterFilterName("Surf");
+		modules.EnableModule("Surf");
+		modules.EnterFilterName("Pub SURF Example");
+		modules.EnableModule("Pub SURF Example");
+		overlay.ClickCloseOverlayLnk();
+		
+		Reporter.log("STEP 10");
+		taxonomy.NavigateSite("Structure>>Blocks");
+		overlay.SwitchToActiveFrame();
+		
+		Reporter.log("STEP 11");
+		Blocks blocks = new Blocks(webDriver);
+		blocks.SelectRegion("Surf Example Actions", "Sidebar first");
+		blocks.SelectRegion("Surf Example Content", "Sidebar first");
+		blocks.ClickSaveBlocksBtn();
+		contentParent.VerifyMessageStatus("The block settings have been updated.");
+		overlay.ClickCloseOverlayLnk();
+		
+		Reporter.log("STEP 12 - N/A");
+		
+		Reporter.log("STEP 13 - N/A");
+		
+		Reporter.log("STEP 14");
+		Surf surf = new Surf(webDriver);
+		surf.ClickSignInBtn();
+		
+		Reporter.log("STEP 15");
+		surf.SwitchToSurfFrm();
+		surf.EnterEmailUsername("brandon.clark@nbcuni.com");
+		surf.EnterPassword("tufNewcyd4#");
+		surf.ClickSurfFrmSignInBtn();
+		overlay.switchToDefaultContent();
+		surf.VerifyUsernamePresent("baclark77");
+		
+		Reporter.log("STEP 16 - N/A");
+		
+		Reporter.log("STEP 17");
+		surf.ClickSignOutBtn();
+		
+		Reporter.log("STEP 18 - N/A");
+		
+		Reporter.log("CLEANUP");
+		taxonomy.NavigateSite("Structure>>Blocks");
+        overlay.SwitchToActiveFrame();
+        blocks.SelectRegion("Surf Example Actions", "- None -");
+        blocks.SelectRegion("Surf Example Content", "- None -");
+        blocks.ClickSaveBlocksBtn();
+        overlay.ClickCloseOverlayLnk();
+        taxonomy.NavigateSite("Modules");
+        overlay.SwitchToActiveFrame();
+        modules.EnterFilterName("Pub SURF Example");
+		modules.DisableModule("Pub SURF Example");
+		modules.EnterFilterName("Surf");
+		modules.DisableModule("Surf");
+		
+	}
+}
