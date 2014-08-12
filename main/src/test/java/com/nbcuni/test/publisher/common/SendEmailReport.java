4809f6be2d8f170920a83bd3debb6210d353df62
baclark77 <baclark77@yahoo.com>
2014-08-12 16:58:32 -0400

    1. line break fix to the send email report.

--- main/src/test/java/com/nbcuni/test/publisher/common/SendEmailReport.java
+++ main/src/test/java/com/nbcuni/test/publisher/common/SendEmailReport.java
@@ -73,7 +73,7 @@ public class SendEmailReport {
             else {
             	messageContent = "<body>Test run complete for latest build against "
                 		+ "<a href='" + config.getConfigValue("AppURL") + "'>" + config.getConfigValue("AppURL") 
-                		+ "</a> on <strong>" + machineName + "</strong>.<br /><br />Test group = " + config.getIncludedGroups() + "<br /><br />Tests passed = " 
+                		+ "</a> on <strong>" + machineName + "</strong>.<br /><br />Test group = " + config.getIncludedGroups() + "<br />Tests passed = " 
                 		+ passedTestsCount.toString() + "<br />Tests failed = " 
                 		+ failedIndividualTestCount.toString() + "<br /><br />A detailed functional report is attached. " 
                 		+ "A detailed http archive file (HAR) is attached and can be viewed with an online <a href='http://www.softwareishard.com/har/viewer/'>HAR viewer</a>. " 
