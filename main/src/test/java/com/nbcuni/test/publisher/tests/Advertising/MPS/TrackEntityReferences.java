da878426a4f8f673fa2ab161e931fcfa3563e432
baclark77 <baclark77@yahoo.com>
2014-10-13 14:05:36 -0400

    1. added new mps entity tracker test. Edited additional information page object to support tags.

---
+++ main/src/test/java/com/nbcuni/test/publisher/tests/Advertising/MPS/TrackEntityReferences.java
@@ -0,0 +1,65 @@
+package com.nbcuni.test.publisher.tests.Advertising.MPS;
+
+import java.util.Arrays;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+import com.nbcuni.test.publisher.common.ParentTest;
+import com.nbcuni.test.publisher.common.RerunOnFailure;
+import com.nbcuni.test.publisher.pageobjects.Modules;
+import com.nbcuni.test.publisher.pageobjects.UserLogin;
+import com.nbcuni.test.publisher.pageobjects.Configuration.MPSConfiguration;
+import com.nbcuni.test.publisher.pageobjects.Content.AdditionalInformation;
+import com.nbcuni.test.publisher.pageobjects.Content.CreateDefaultContent;
+import com.nbcuni.test.publisher.pageobjects.Content.WorkBench;
+
+public class TrackEntityReferences extends ParentTest {
+	
+    /*************************************************************************************
+     * TEST CASE - TC5445
+     * Steps - https://rally1.rallydev.com/#/14663927728d/detail/testcase/23568460386
+     *************************************************************************************/
+    @Test(retryAnalyzer = RerunOnFailure.class, groups = {"full", "mps"})
+    public void TrackEntityReferences_TC5445() throws Exception {
+        
+        	Reporter.log("STEP 1");
+        	UserLogin userLogin = applib.openApplication();
+        	userLogin.Login(config.getConfigValueString("Admin1Username"), config.getConfigValueString("Admin1Password"));
+            
+        	Reporter.log("SETUP");
+        	Modules modules = new Modules(webDriver);
+        	modules.VerifyModuleEnabled("MPS");
+        	CreateDefaultContent createDefaultContent = new CreateDefaultContent(webDriver);
+        	String tvShowTitle = createDefaultContent.TVShow("Draft");
+        	String tvSeasonTitle = createDefaultContent.TVSeason("Draft", tvShowTitle);
+        	String tvEpisodeTitle = createDefaultContent.TVEpisode("Draft", tvShowTitle, tvSeasonTitle);
+        	WorkBench workBench = new WorkBench(webDriver);
+        	workBench.ClickWorkBenchTab("Edit Draft");
+        	overlay.SwitchToActiveFrame();
+        	AdditionalInformation additionalInformation = new AdditionalInformation(webDriver);
+        	additionalInformation.ClickAdditionalInformationLnk();
+        	additionalInformation.EnterTag("tag1");
+        	additionalInformation.EnterTag("tag2");
+        	contentParent.ClickSaveBtn();
+        	overlay.switchToDefaultContent(true);
+        	
+        	Reporter.log("STEP 2");
+        	taxonomy.NavigateSite("Configuration>>Web services>>MPS Configuration");
+            overlay.SwitchToActiveFrame();
+            MPSConfiguration mpsConfiguration = new MPSConfiguration(webDriver);
+            mpsConfiguration.EnterMPSHost("mps.io");
+            mpsConfiguration.ClickIntegrationMethod("Document Write");
+            mpsConfiguration.EnterSiteInstanceOverride("pub7-development");
+            mpsConfiguration.CheckSendQueryStringsCbx();
+            mpsConfiguration.CleanAllMPSOptions();
+            mpsConfiguration.ClickSaveConfigurationBtn();
+            contentParent.VerifyMessageStatus("The configuration options have been saved.");
+            overlay.ClickCloseOverlayLnk();
+            
+            Reporter.log("STEP 3");
+            applib.openSitePage("/content/" + tvEpisodeTitle);
+            contentParent.VerifySourceInPage(Arrays.asList("\"cag\":{\"tags\":\"tag1|tag2\",\"show\":\"" + tvShowTitle + "\",\"season\":\"" + tvSeasonTitle + "\"}"));
+            
+            
+            
+    }
+}
