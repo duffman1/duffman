14ddab9aa026676a10d05cd7942942a2d585cc3e
Brandon Clark <baclark77@yahoo.com>
2015-05-26 15:37:42 -0400

    updated select file as needed with temp wait

--- main/src/test/java/com/nbcuni/test/publisher/pageobjects/Content/SelectFile.java
+++ main/src/test/java/com/nbcuni/test/publisher/pageobjects/Content/SelectFile.java
@@ -136,7 +136,7 @@ public class SelectFile {
     	errorChecking.VerifyNoMessageErrorsPresent();
     	
     	//TODO - this section is for a tricky chrome driver select file frame issue with drupal
-    	//it should go away as soon as possible
+    	//this ugliness should be refactored as soon as possible
     	webDriver.switchTo().defaultContent();
     	Thread.sleep(10000);
     	webDriver.switchTo().frame(waitFor.ElementPresent(SelectFile_Frm));
